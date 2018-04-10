f1798cfe1fa6a4acd4b7aac534c958e1edfb8ff1
victorbofill <victorm.bofill@gmail.com>
2018-04-10 14:11:23 -0700

    All tests working

--- test/app.test.js
+++ test/app.test.js
@@ -15,6 +15,7 @@ describe('Server Logging', () => {
     beforeEach(done => {
         client1 = net.connect(PORT, () => {
             client1.setEncoding('utf8');
+            client1.write(message1);
             done();
         });
     });
@@ -24,6 +25,7 @@ describe('Server Logging', () => {
     beforeEach(done => {
         client2 = net.connect(PORT, () => {
             client2.setEncoding('utf8');
+            client2.write(message2);
             done();
         });
     });
@@ -38,30 +40,48 @@ describe('Server Logging', () => {
     });
 
     let logFile;
+    let firstMessage;
+    let secondMessage;
 
-    it('Client message is logged to server-log.txt', done => {
-        client1.write(message1);
-        client2.write(message2);
-
+    it('First message is successfully logged to server-log.txt', done => {
         setTimeout(() => {
             fs.readFile('./test/server-log.txt', 'utf8', (err, data) => {
                 if (err) throw err;
                 logFile = data;
             });
         }, 30);
-    
+
         setTimeout(() => {
             const firstSplit = logFile.split('\n');
-            const firstMessage = firstSplit[0].split(' ** ');
-            const secondMessage = firstSplit[1].split(' ** ');
+            firstMessage = firstSplit[0].split(' ** ');
     
             assert.deepEqual(firstMessage[1], message1);
-            assert.deepEqual(secondMessage[1], message2);
+        }, 50);
+        done();
+    });
+    
+    it('Second message is successfully logged to server-log.txt', done => {
+        setTimeout(() => {
+            fs.readFile('./test/server-log.txt', 'utf8', (err, data) => {
+                if (err) throw err;
+                logFile = data;
+            });
+        }, 30);
 
+        setTimeout(() => {
+            const firstSplit = logFile.split('\n');
+            secondMessage = firstSplit[1].split(' ** ');
+    
+            assert.deepEqual(secondMessage[1], message2);
+        }, 50);
+        done();
+    });
+
+    it('Time is correctly logged', done => {
+        setTimeout(() => {
             const d = new Date(firstMessage[0]);
             isNaN(d.getTime());
-        }, 50);
-
+        }, 80);
         done();
     });
 });
